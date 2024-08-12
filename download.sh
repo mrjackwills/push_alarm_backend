4c142b9f321306ca10dff74c0e88db5bdd927571
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2024-08-12 14:47:40 +0000

    fix: download.sh typo

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-case "$(arch)" in
+UNAME_CMD="$(uname -m)"
+case "$UNAME_CMD" in
 x86_64) SUFFIX="x86_64" ;;
 aarch64) SUFFIX="aarch64" ;;
 armv6l) SUFFIX="armv6" ;;
@@ -8,7 +9,7 @@ esac
 
 if [ -n "$SUFFIX" ]; then
 	PUSH_ALARM_GZ="push_alarm_linux_${SUFFIX}.tar.gz"
-	wget "https://github.com/mrjackwills/psuh_alarm_backend/releases/latest/download/${PUSH_ALARM_GZ}"
+	curl -L -O "https://github.com/mrjackwills/push_alarm_backend/releases/latest/download/${PUSH_ALARM_GZ}"
 	tar xzvf "${PUSH_ALARM_GZ}" push_alarm
 	rm "${PUSH_ALARM_GZ}"
 fi
