a9ad358122bd2029c91f62660976ef026d181026
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-10-23 06:38:31 +0000

    fix: correctly randomise oblique  strategy

--- src/db/model_oblique_strategy.rs
+++ src/db/model_oblique_strategy.rs
@@ -2,7 +2,10 @@ use sqlx::{Sqlite, SqlitePool, Transaction};
 
 use crate::app_error::AppError;
 
-pub struct ModelObliqueStrategy;
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash, sqlx::FromRow)]
+pub struct ModelObliqueStrategy {
+    pub strategy: String,
+}
 
 impl ModelObliqueStrategy {
     async fn insert(
@@ -16,6 +19,15 @@ impl ModelObliqueStrategy {
             .execute(&mut **transaction)
             .await?;
         Ok(())
+    }
+
+    pub async fn get_random(sqlite: &SqlitePool) -> Result<String, AppError> {
+        Ok(
+            sqlx::query_as::<_, Self>("SELECT * FROM oblique_strategies ORDER BY RANDOM() LIMIT 1")
+                .fetch_one(sqlite)
+                .await?
+                .strategy,
+        )
     }
 
     pub async fn seed_stratergies(sqlite: &SqlitePool) -> Result<(), AppError> {
@@ -34,6 +46,8 @@ impl ModelObliqueStrategy {
 #[expect(clippy::unwrap_used)]
 mod tests {
 
+    use std::collections::HashSet;
+
     use crate::{
         db::{create_tables, file_exists, get_db},
         tests::{gen_app_envs, test_cleanup},
@@ -49,17 +63,47 @@ mod tests {
         let app_envs = gen_app_envs(uuid);
 
         file_exists(&app_envs.location_sqlite);
-        let db = get_db(&app_envs).await.unwrap();
-        create_tables(&db).await;
+        let sqlite = get_db(&app_envs).await.unwrap();
+        create_tables(&sqlite).await;
 
-        let result = ModelObliqueStrategy::seed_stratergies(&db).await;
+        let result = ModelObliqueStrategy::seed_stratergies(&sqlite).await;
 
         assert!(result.is_ok());
 
         // Make sure conflicts are ignored, in that they don't cause errors
-        let result = ModelObliqueStrategy::seed_stratergies(&db).await;
+        let result = ModelObliqueStrategy::seed_stratergies(&sqlite).await;
 
         assert!(result.is_ok());
-        test_cleanup(uuid, Some(db)).await;
+        test_cleanup(uuid, Some(sqlite)).await;
+    }
+
+    #[tokio::test]
+    async fn model_oblique_trategy_get() {
+        let uuid = Uuid::new_v4();
+
+        let app_envs = gen_app_envs(uuid);
+
+        file_exists(&app_envs.location_sqlite);
+        let sqlite = get_db(&app_envs).await.unwrap();
+        create_tables(&sqlite).await;
+
+        ModelObliqueStrategy::seed_stratergies(&sqlite)
+            .await
+            .unwrap();
+
+        let mut set = HashSet::new();
+
+        for _ in 0..=40 {
+            let result = ModelObliqueStrategy::get_random(&sqlite).await;
+            assert!(result.is_ok());
+            let result = result.unwrap();
+            assert!(!result.is_empty());
+            set.insert(result);
+        }
+
+        // This could fail
+        assert!(set.len() > 15);
+
+        test_cleanup(uuid, Some(sqlite)).await;
     }
 }
